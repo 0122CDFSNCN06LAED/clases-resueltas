a6c772e7d20b7616064deaeb6fe717b13efe0c83
Pablo Baleztena <pablo@ultharsoftware.com>
2022-03-30 19:08:44 -0300

    clase-17

---
+++ 17-html/app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.listen(3000);
+
+app.use(express.static(path.join(__dirname, "public")));
+
+//localhost:3000/
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "views/home.html"));
+});
+
+app.get("/register", (req, res) => {
+  res.sendFile(path.join(__dirname, "views/register.html"));
+});
+
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "views/login.html"));
+});
